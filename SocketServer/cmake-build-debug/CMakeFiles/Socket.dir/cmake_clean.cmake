596034c140473cedb0bf68e958eaf8e90d7fcc32
drizzledrop3 <95005094+drizzledrop3@users.noreply.github.com>
2023-04-08 16:41:06 +0800

    Add files via upload

---
+++ SocketServer/cmake-build-debug/CMakeFiles/Socket.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Socket.dir/main.cpp.obj"
+  "Socket.exe"
+  "Socket.exe.manifest"
+  "Socket.lib"
+  "Socket.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Socket.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
